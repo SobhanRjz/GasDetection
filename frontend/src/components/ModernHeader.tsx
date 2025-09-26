fa171378dea7e92169e965ea2668a88801b50a64
SobhanRjz <sobhan.rajabzadeh@gmail.com>
2025-09-26 03:37:06 +0330

    FIX remove icon

--- frontend/src/components/ModernHeader.tsx
+++ frontend/src/components/ModernHeader.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 // Simple inline SVG icons since lucide-react installation had issues
-const BrandIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="12" cy="12" r="10" fill="#8B5CF6"/>
-    <path d="M8 12L11 15L16 9" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-  </svg>
-);
 
 const ArrowRightIcon = () => (
   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
